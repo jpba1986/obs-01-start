75092e7ba93081d156021dc22ddf98bf7e5efcc0
unknown <juanpablo.aguila@gmail.com>
2019-07-22 22:24:29 -0400

    Observables: subscribe and  unsubscribe

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy} from '@angular/core';
 
+import { interval, Subscription } from 'rxjs';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  private firstObsSubscription: Subscription;
+
   constructor() { }
 
   ngOnInit() {
+    this.firstObsSubscription = interval(1000).subscribe(
+      (count) =>{
+        console.log(count);
+      }
+    );
   }
 
+  ngOnDestroy(): void {
+    this.firstObsSubscription.unsubscribe();
+  }
 }
